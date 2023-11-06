e7b18e7d0c6f2b1a798e2d024fbf29251cf294a6
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-11-06 20:44:45 +0300

    push file

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/marsh-sudo/go-jwt/controllers"
+	"github.com/marsh-sudo/go-jwt/initializers"
+	"github.com/marsh-sudo/go-jwt/middleware"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/signup", controllers.SignUp) 
+	r.POST("/login",controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
