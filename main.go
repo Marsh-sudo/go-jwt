0f829fb23cfdf540b9c693e5021c621f3e2a7375
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-11-28 18:44:56 +0300

    create ping route

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.SignUp) 
 	r.POST("/login",controllers.Login)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
